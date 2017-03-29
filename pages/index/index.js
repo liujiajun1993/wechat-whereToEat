0023aa937e1e2940914158e7f36728290987e88e
LIUJIAJUN-PC\liujiajun <liujiajun1993@gmail.com>
2017-03-29 22:09:29 +0800

    ver1.1，增加食堂的倍率选择，不同食堂可以拥有不同的选中概率

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,59 @@ Page({
     schoolList:['北京大学'],
     schoolSelected: '北京大学',
     refectoryList:{
-      '北京大学': ['农园一层', '农园二层', '燕南', '学一', '学五', '勺园', '艺园', '畅春新园']
+      '北京大学': [{
+        name: '农园一层',
+        ratio: 1
+      }, {
+        name: '农园二层',
+        ratio: 1
+      },{
+        name: '燕南',
+        ratio: 1
+      },{
+        name: '学一',
+        ratio: 1
+      },{
+        name: '学五',
+        ratio: 1
+      },{
+        name: '勺园',
+        ratio: 1
+      },{
+        name: '艺园',
+        ratio: 1
+      },{
+        name: '畅春新园',
+        ratio: 1
+      }]
     },
-    refectorys: ['农园一层', '农园二层', '燕南', '学一', '学五', '勺园', '艺园', '畅春新园'],
+    refectorys: [{
+        name: '农园一层',
+        ratio: 1
+      }, {
+        name: '农园二层',
+        ratio: 1
+      },{
+        name: '燕南',
+        ratio: 1
+      },{
+        name: '学一',
+        ratio: 1
+      },{
+        name: '学五',
+        ratio: 1
+      },{
+        name: '勺园',
+        ratio: 1
+      },{
+        name: '艺园',
+        ratio: 1
+      },{
+        name: '畅春新园',
+        ratio: 1
+      }],
     selectedRefectory: null,
+    ratioList: ['1倍概率','2倍概率','3倍概率','4倍概率','5倍概率'],
     userInfo: {}
   },
   //事件处理函数
@@ -34,16 +83,30 @@ Page({
   },
   // 随机食堂
   onStartSelect: function(){
-    var totalCount = this.data.refectorys.length;
-    var randomNum = Math.floor(Math.random()*totalCount);
-    var currentRefectory = this.data.refectorys[randomNum];
+    var totalCount = 0, selectedIndex = 0;
+    var tempRefectorys = this.data.refectorys;
+    tempRefectorys.forEach(
+      function(item){
+        totalCount += item.ratio;
+      }
+    );
+    var randomNum = Math.floor(Math.random()*totalCount) + 1;
+    for(let i = 0, len = tempRefectorys.length; i < len; i++){
+      randomNum -= tempRefectorys[i].ratio;
+      if(randomNum <= 0){
+        selectedIndex = i;
+        break;
+      }
+    }
+    var currentRefectory = tempRefectorys[selectedIndex];
     this.setData({
       selectedRefectory: currentRefectory
     });
   },
   // 删除当前选项并重新随机
   onDeleteandSelect: function(){
-    var index = this.data.refectorys.indexOf(this.data.selectedRefectory);
+    var name = this.data.selectedRefectory.name;
+    var index = this.data.refectorys.findIndex(function(element){return element.name == name});
     var currentArr = this.data.refectorys;
     currentArr.splice(index, 1);
     this.setData({
@@ -54,12 +117,27 @@ Page({
   },
   // 食堂列表中删除选项
   onDeleteOption: function(e){
-    var index = this.data.refectorys.indexOf(e.target.dataset.refectory);
+    var name = e.target.dataset.refectory;
+    var index = this.data.refectorys.findIndex(function(element){return element.name == name});
     var currentArr = this.data.refectorys;
     currentArr.splice(index, 1);
     this.setData({
       // 注意此处，如果重新选择学校，那么删除选项将变回来
       refectorys: currentArr
+    });
+  },
+  // 更改倍率
+  onRatioChange: function(e){
+    var ratio = +e.detail.value + 1;
+    var name = e.target.dataset.refectory;
+    var tempRefectorys = this.data.refectorys;
+    tempRefectorys.forEach(function(item){
+      if(item.name == name){
+        item.ratio = ratio;
+      }
+    });
+    this.setData({
+      refectorys: tempRefectorys
     });
   }
 })
